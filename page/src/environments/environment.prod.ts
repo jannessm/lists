d813f77a2bb5073002a5291b5dfe18c776c2ce65
Jannes Magnusson <j-magnusson@t-online.de>
2024-03-28 08:12:35 +0100

    update prod url

--- page/src/environments/environment.prod.ts
+++ page/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { version } from './version';
 
 export const environment = {
   production: true,
-  api: 'https://listsapp.de/api/',
+  api: 'https://lists.magnusso.nz/api/',
   locale: 'de',
   version: version,
   vapid: 'BEQ7Z6AYftjPVg8a554wmJUFeCeR5UAs4eBqWQOUFbbwlbK6qjlbo3TR7GwgpfhJ4TAT2-sGZZQMwkVXRQEdxOI'
