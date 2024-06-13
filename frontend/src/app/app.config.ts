31e7ba4c17dabdbb4a03e07f83b80712c4b7d50d
Jannes Magnusson <j-magnusson@t-online.de>
2024-06-13 22:25:14 +0200

    upgrade ng

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,6 +1,6 @@
 import { ApplicationConfig, importProvidersFrom, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { HttpClientModule, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
